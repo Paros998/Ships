84a68a37b2cfe47ffa6927f61493fb1b188595d4
Paros998 <73112078+Paros998@users.noreply.github.com>
2021-03-02 13:12:58 +0100

    aplikacja dziala

--- core/src/com/ourshipsgame/Main.java
+++ core/src/com/ourshipsgame/Main.java
@@ -1,7 +1,18 @@
 package com.ourshipsgame;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 public class Main {
     public static void main(String[] args) {
-
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Ships!";
+        config.width = 1920;
+        config.height = 1080;
+        config.fullscreen = false;
+        config.foregroundFPS = 60;
+        config.resizable = true;
+        System.out.println("App launched w:" + config.width + " h:" + config.height);
+        new LwjglApplication(new MyGame(), config);
     }
 }
--- desktop/src/com/ourshipsgame/desktop/DesktopLauncher.java
+++ desktop/src/com/ourshipsgame/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.ourshipsgame.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.ourshipsgame.App;
+import com.ourshipsgame.MyGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new App(), config);
+		new LwjglApplication(new MyGame(), config);
 	}
 }
