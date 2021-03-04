3c4351316da40f3bd0bd6b4a2e9b96429f352337
Paros998 <73112078+Paros998@users.noreply.github.com>
2021-03-04 15:17:52 +0100

    up gameobject

--- core/src/com/ourshipsgame/GameObject.java
+++ core/src/com/ourshipsgame/GameObject.java
@@ -1,6 +1,7 @@
 package com.ourshipsgame;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
 
 public class GameObject extends Rectangle {
@@ -9,17 +10,24 @@ public class GameObject extends Rectangle {
      */
     private static final long serialVersionUID = 1L;
     protected Texture texture;
+    protected Sprite sprite;
 
-    public GameObject(String internalPath, float x, float y) {
+    public GameObject(String internalPath, float x, float y, boolean createSprite) {
         texture = new Texture(internalPath);
         this.setX(x);
         this.setY(y);
         this.width = texture.getWidth();
         this.height = texture.getHeight();
+        if (createSprite)
+            createSprite(texture);
     }
 
     public void moveTexture(float x) {
         this.x += x;
+    }
+
+    protected void createSprite(Texture texture) {
+        this.sprite = new Sprite(texture);
     }
 
 }
