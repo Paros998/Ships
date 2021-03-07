3c1a8e1ec749bec297d7622f9fc011bb1fb2a1f9
Dominik <dominik.grudzien00@gmail.com>
2021-03-07 21:41:32 +0100

    Poprawa MenuScreen i GameButton

--- core/src/com/ourshipsgame/GameButton.java
+++ core/src/com/ourshipsgame/GameButton.java
@@ -1,15 +1,55 @@
 package com.ourshipsgame;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class GameButton extends TextButton {
 
     public Skin skin;
+    Game game;
 
-    public GameButton(String nameTag, float x, float y, Skin skin) {
+    public GameButton(String nameTag, float x, float y, Skin skin, final int buttonNumber, final Game game) {
         super(nameTag, skin);
+        this.game = game;
         this.setX(x - this.getWidth() / 2);
         this.setY(y);
+
+        this.addListener(new InputListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                menuOptions(buttonNumber);
+            }
+            
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+        });
     }
+
+    private void menuOptions(int option) {
+		switch(option) {
+		case 1:
+            game.setScreen(new GameScreen(game));
+			break;
+			
+		case 2:
+			break;
+			
+		case 3:
+			break;
+			
+		case 4:
+			break;
+			
+		case 5:
+			Gdx.app.exit();
+			break;
+		}
+	}
+
 }
--- core/src/com/ourshipsgame/MenuScreen.java
+++ core/src/com/ourshipsgame/MenuScreen.java
@@ -2,19 +2,15 @@ package com.ourshipsgame;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.ourshipsgame.handlers.Constant;
 
-public class MenuScreen extends ScreenAdapter implements Constant {
-
-    private final String id = getClass().getName();
+public class MenuScreen implements Screen, Constant {
 
     // vars mandatory
     private Game game;
@@ -29,7 +25,6 @@ public class MenuScreen extends ScreenAdapter implements Constant {
 
     public MenuScreen(Game game) {
         this.game = game;
-        Gdx.app.log(id, "This class is loaded!");
     }
 
     private void moveMenu(float deltaTime) {
@@ -55,61 +50,11 @@ public class MenuScreen extends ScreenAdapter implements Constant {
 
         // Buttons
         skin = new Skin(Gdx.files.internal("core/assets/buttons/skins/rusty-robot/skin/rusty-robot-ui.json"));
-        playButton = new GameButton("Play", GAME_WIDTH / 2, GAME_HEIGHT / 2 + 100, skin);
-        helpButon = new GameButton("Help", GAME_WIDTH / 2, GAME_HEIGHT / 2, skin);
-        scoreButton = new GameButton("Score", GAME_WIDTH / 2, GAME_HEIGHT / 2 - 100, skin);
-        optionsButton = new GameButton("Options", GAME_WIDTH / 2, GAME_HEIGHT / 2 - 200, skin);
-        quitButton = new GameButton("Exit", GAME_WIDTH / 2, GAME_HEIGHT / 2 - 300, skin);
-
-        playButton.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                return true;
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                processEnter(4);
-            }
-        });
-
-        helpButon.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                return true;
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                processEnter(3);
-            }
-        });
-
-        scoreButton.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                return true;
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                processEnter(2);
-            }
-        });
-
-        quitButton.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                if (event.isTouchFocusCancel())
-                    processEnter(1);
-                return true;
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-
-            }
-        });
+        playButton = new GameButton("Play", GAME_WIDTH / 2, GAME_HEIGHT / 2 + 100, skin, 1, game);
+        helpButon = new GameButton("Help", GAME_WIDTH / 2, GAME_HEIGHT / 2, skin, 2, game);
+        scoreButton = new GameButton("Score", GAME_WIDTH / 2, GAME_HEIGHT / 2 - 100, skin, 3, game);
+        optionsButton = new GameButton("Options", GAME_WIDTH / 2, GAME_HEIGHT / 2 - 200, skin, 4, game);
+        quitButton = new GameButton("Exit", GAME_WIDTH / 2, GAME_HEIGHT / 2 - 300, skin, 5, game);
 
         stage.addActor(playButton);
         stage.addActor(helpButon);
@@ -118,52 +63,22 @@ public class MenuScreen extends ScreenAdapter implements Constant {
         stage.addActor(quitButton);
     }
 
-    private void handleButtons() {
-        if (quitButton.isPressed())
-            Gdx.app.exit();
-    }
-
-    private void processEnter(int option) {
-        // Changing screen based on screenInt and Enter pressed
-        switch (option) {
-            case 4: {
-                super.dispose();
-                game.setScreen(new GameScreen(game));
-                break;
-            }
-            case 3: {
-                // super.dispose();
-                // game.setScreen(new OptionScreen(game));
-                break;
-            }
-            case 2: {
-                // super.dispose();
-                // game.setScreen(new ScoreScreen(game));
-                break;
-            }
-            case 1: {
-                Gdx.app.exit();
-                break;
-            }
-
-        }
-    }
-
     // update logics method
     private void update(float deltaTime) {
         moveMenu(deltaTime);
-        handleButtons();
+        stage.act();
     }
 
     // game loop method
     @Override
     public void render(float deltaTime) {
+        // update
+        update(deltaTime);
+
         // buffer screen
         Gdx.gl20.glClearColor(1, 1, 1, 1);
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act();
-        // update
-        update(deltaTime);
+        
         // render things
         sb.begin();
         sb.draw(menuTexture.texture, menuTexture.x, menuTexture.y);
@@ -175,17 +90,33 @@ public class MenuScreen extends ScreenAdapter implements Constant {
     public void show() {
         sb = new SpriteBatch();
         createGraphics();
-
-        Gdx.app.log(id, "The game is running");
     }
 
     @Override
     public void pause() {
-        Gdx.app.log(id, "The game is paused");
     }
 
     @Override
     public void resume() {
-        Gdx.app.log(id, "The game is resumed");
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void dispose() {
+        sb.dispose();
+        stage.dispose();
+        skin.dispose();
+        System.out.println("Elements from main menu disposed.");
     }
 }
